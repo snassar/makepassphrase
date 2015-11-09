6993f93d92161a98a888c13b84e41f1d763e06d5
Samir Nassar <snassar@users.noreply.github.com>
2015-11-09 20:38:56 +0100

    Added proper GPL notice to source code

--- makepassphrase.sh
+++ makepassphrase.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+
+# makepassphrase - a passphrase generator
+# Copyright (C) 2015  Samir Nassar
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 passphrases=$1
 
 # Generates passphrase array with seven words.
